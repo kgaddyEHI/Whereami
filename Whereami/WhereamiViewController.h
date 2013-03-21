82da3b82569ab2bf0082de55dfa14ee3e87687e2
Kevin Gaddy <kevin.gaddy@gmail.com>
2013-03-21 16:54:23 -0500

    init
    Signed-off-by: Kevin Gaddy <kevin.gaddy@gmail.com>

---
+++ Whereami/WhereamiViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhereamiViewController.h
+//  Whereami
+//
+//  Created by kgaddy on 3/21/13.
+//  Copyright (c) 2013 com.kgaddy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WhereamiViewController : UIViewController
+
+@end
---
+++ Whereami/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Whereami
+//
+//  Created by kgaddy on 3/21/13.
+//  Copyright (c) 2013 com.kgaddy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WhereamiAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WhereamiAppDelegate class]));
+    }
+}
